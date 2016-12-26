71030a0376d4a3d9838bf6cd16ee789e4fd69a35
freemind122 <freemind122@hotmail.co.uk>
2016-12-26 20:19:29 +0000

    Create Notes.js

---
+++ Notes.js
@@ -0,0 +1,22 @@
+/*
+NOTES ON IMPROVEMENTS
+
+-   Want to find a way of making the Behaviour script use the "role" attribute 
+    of the Units module
+    
+-   Governor function is currently catching an error on the harvester behaviour
+    I would like to get the try working, meaning that I would like to get units
+    behaviour comming from the particular unit's attributes found in the 
+    "Units" module
+    
+-   Want to find a way of having multiple variables within a module with
+    different functions as attributes
+
+NOTES ON DISCOVERIES
+
+-   It does not seem that you can have 2 variables in a module and call on an
+    attribute as a function, however, you may have multiple functions as 
+    attributes of the same variable
+
+
+
