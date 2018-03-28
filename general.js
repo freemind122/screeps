f524243b51ec35e80c428472c60113ac4ef353a5
Marcus <freemind122@hotmail.co.uk>
2018-03-28 09:53:12 +0100

    testing source clear function

--- general.js
+++ general.js
@@ -44,6 +44,7 @@ var custom = {
                     )
                 });
                 var test = source.Room.lookForAt(LOOK_CREEPS,area[0].x,area[0].y);
+                console.log('1')
                 if (test[0] !== undefined) {
                     console.log('Creep in empty space - x:' + area[0].x + ' y:' + area[0].y)
                 }
